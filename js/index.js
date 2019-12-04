e827f4cd92abe12a2bcf09c0db95ccec0c2fe0f9
Jacob Prudent <jake.prudent@gmail.com>
2019-12-03 17:43:14 -0800

    Met MVP. Took a while so I am not stretching today

--- js/index.js
+++ js/index.js
@@ -1,1 +1,89 @@
 // Your code goes here
+
+//define objects
+const busImg = document.querySelector(".intro img")
+const logo = document.querySelector(".logo-heading")
+const hback = document.querySelector(".main-navigation")
+const textish =document.querySelectorAll(".text-content *")
+const body = document.querySelector(`body`)
+const welcome = document.querySelector(`.intro h2`)
+const intro = document.querySelector(`.intro`)
+
+//some functions I looked up for fun
+const randcolor = function(){return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);}
+
+//Events
+//mouseover
+busImg.addEventListener("mouseover", ()=>{
+    busImg.style.filter ="invert()"
+})
+busImg.addEventListener("mouseleave", ()=>{
+    busImg.style.filter ="none";
+    console.log("Activated moveleave");
+} )
+    //click preventing propogation
+intro.addEventListener("click",(()=>{
+    intro.style.backgroundColor = `red`;
+    event.stopPropagation();
+}))
+body.addEventListener(`click`,(()=>{
+    body.style.background =`yellow`;
+    
+
+}))   
+//keydown
+document.addEventListener("keydown", ()=> {
+logo.style.color = "yellow";
+console.log("Activated keydown");
+})
+document.addEventListener("keyup",()=>{
+    logo.style.background = "black";
+console.log("Activated Keyup");
+} )
+//wheel
+document.addEventListener("wheel",()=>{
+    hback.style.filter = "opacity(70)";
+    hback.style.backgroundColor= randcolor();
+    console.log("Activated wheel")
+})
+// `drag / drop`
+document.addEventListener("drag",()=>{
+    busImg.style.transform ="scale(2)";
+})
+document.addEventListener("dragend",()=>{
+    busImg.style.transform = "scale(1)";
+})
+//copy
+body.addEventListener(`copy`,()=>{
+    busImg.style.transform = "scale(.75)";
+    console.log("Activated copy")
+})
+
+//offline
+window.addEventListener(`offline`,(() =>{
+    busImg.style.border= `black solid 1px`;
+    console.log(`The network is offline`);
+}))
+
+//resize
+window.addEventListener(`resize`,(()=>{
+  logo.style.border = `black solid 1px`;
+  console.log(`window resized`)  
+}))
+// 	`scroll`
+window.addEventListener(`scroll`,(()=>{
+
+    busImg.style.border = `${randcolor()} solid 5px`;
+    console.log("scroll!")
+}))
+// `contextmenu` 
+   body.addEventListener(`contextmenu`, ()=>{
+    alert(`I see you!`);
+    console.log("Activated contextmenu")
+    });
+    //console.log(el)
+//  `dblclick`
+busImg.addEventListener(`dblclick`, ()=>{
+    busImg.style.transform= `scale(.75)`;
+    console.log("Activated Clicky")
+})
